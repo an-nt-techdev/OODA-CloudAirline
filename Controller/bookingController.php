bdd2b49cc6937f7217a1b0586d362b02a991873f
Thiên Ân <ndsg1964@gmail.com>
2019-11-07 16:46:42 +0700

    Sửa lần 21

--- Controller/bookingController.php
+++ Controller/bookingController.php
@@ -6,17 +6,9 @@
 
     $p='home';
 
-<<<<<<< Updated upstream
 
-     if (isset($_GET['checkout'])) require_once SITE_ROOT.'/View/checkout.php';
-=======
-    if (isset($_GET['choose_seat'])) 
-    {
-        
-        require_once SITE_ROOT.'/View/choose_seat.php';
-    }
-    else if (isset($_GET['checkout'])) require_once SITE_ROOT.'/View/checkout.php';
->>>>>>> Stashed changes
+
+    if (isset($_GET['checkout'])) require_once SITE_ROOT.'/View/checkout.php';
     else if (isset($_GET['checking'])) require_once SITE_ROOT.'/View/checking.php';
     else 
     {
